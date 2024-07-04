16929476527811f18df4ecf2f00cefc90584a846
throwawaosu <toshiotrenchard@gmail.com>
2024-07-04 09:50:01 -0600

    mudblazor things

--- BookWebApp/Components/BookSystem/BookSystem/Author.cs
+++ BookWebApp/Components/BookSystem/BookSystem/Author.cs
@@ -41,22 +41,6 @@ namespace BookSystem
 
         #endregion
         #region methods
-        public static Author[] baseAuthors()
-        {
-            List<Author> authors = new List<Author>();
-
-
-            Author author1 = new Author("Karen Miller");
-            Author author2 = new Author("Adrian Tchaikovsky");
-            Author author3 = new Author("Cixin Liu");
-
-            authors.Add(author1);
-            authors.Add(author2);
-            authors.Add(author3);
-            return authors.ToArray();
-            /// I havea  feeling this is probably where i would need the base classes too bad i can't find them and i cannot be bothered to look 
-        }
-
 
         #endregion 
     }
